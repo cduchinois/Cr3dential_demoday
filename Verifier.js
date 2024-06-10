7db71a179e36bd73c8276a39b05474a42609f9a0
anfal.bourouina <anfal.bourouina@qonto.com>
2024-06-10 10:22:59 +0200

    clean repo

--- Verifier.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("Verifier", (m) => {
-
-  const verifier = m.contract("Verifier");
-
-
-  return { verifier };
-});
